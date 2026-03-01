f50925da56639abb1e2714d22f48c24c0a98815b
Admin <khandwenilesh@gmail.com>
2026-03-01 23:51:43 +0530

    working code

--- src/test/java/org/example/TestRunner.java
+++ src/test/java/org/example/TestRunner.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.StepDefinitions")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example")
 public class TestRunner {
     // empty - JUnit Platform runs Cucumber features located under classpath "features"
 }
